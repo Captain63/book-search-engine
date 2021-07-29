81d691944eac8cfd407c643c2541bc40b5633858
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-07-29 17:03:12 -0400

    Update data returned by mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,13 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-    mutation login($username: String!, $email: String!, $password: String!) {
-        login(username: $username, email: $email, password: $password) {
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
             token
+            user {
+                _id
+                username
+            }
         }
     }  
 `;
@@ -12,14 +16,22 @@ export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
+            user {
+                _id
+                username
+            }
         }
     }
 `;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook($saveBook: BookInput!) {
-        saveBook(saveBook: $saveBook) {
+    mutation saveBook($book: BookInput!) {
+        saveBook(book: $book) {
+            username
             bookCount
+            savedBooks {
+                bookId
+            }
         }
     }
 `;
@@ -27,7 +39,15 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: ID!) {
         removeBook(bookId: $bookId) {
+            username
             bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+            }
         }
     }
 `;
